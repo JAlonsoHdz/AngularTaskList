cb3daaf2fd51d35988858c85e8cca885d01d2c66
Alonso Hernandez <jesus.alonso.hernandez@gmail.com>
2019-04-15 17:03:37 -0500

    Built List records view and setup Person Model

--- src/app/Service/service.service.ts
+++ src/app/Service/service.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from 'selenium-webdriver/http';
+import {Person} from '../Model/Person';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  
+
+  constructor(private http:HttpClient) { }
+
+  Url = 'http://localhost:8080/sample/person';
+
+  getPersons() {
+    return this.http.get<Person[]>(this.Url);
+  }
 }
